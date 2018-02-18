ab894ee584c50bb8864dd9a395323f1c383fbdb7
Eslam nasser <in1146@fayoum.edu.eg>
2018-02-18 21:12:14 +0200

    finish the project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-// Open and close tools
+// Global Vars
 var toggleToolsButton = document.getElementsByClassName('toggle-tools')[0];
 var tools = document.getElementsByClassName('tools')[0];
+var mouse = document.getElementById('mouse');
+
 
 toggleToolsButton.addEventListener('click', function () {
     tools.classList.add('open');
@@ -8,24 +10,46 @@ toggleToolsButton.addEventListener('click', function () {
 
 
 // Step #1: Handle select single tool
-var selectedTool;
-
 var toolsButtons = document.querySelectorAll('.single-tool');
 for (var i = 0; i < toolsButtons.length; i++) {
     toolsButtons[i].addEventListener('click', function () {
         var src = this.children[0].src;
-        selectedTool = src;
+        // Step #3: Change #mouse background on click to the src var
+        mouse.style.backgroundImage = `url('${src}')`;
     });
 }
 
 
 
+// Step #2: Track mouse location and add sample
+window.addEventListener('mousemove', function (moveDetails) {
+    var x = moveDetails.clientX;
+    var y = moveDetails.clientY;
+
+    mouse.style.top = y + 'px';
+    mouse.style.left = x + 'px';
+});
 
 
+// Step #2: Track mouse location and add sample
+var spaceLayer = document.getElementsByClassName('spacetime')[0];
+spaceLayer.addEventListener('click', function (moveDetails) {
+    var x = moveDetails.clientX;
+    var y = moveDetails.clientY;
 
+    var clone = document.createElement('div');
+    clone.classList.add('clone');
+    clone.style.backgroundImage = mouse.style.backgroundImage
 
+    clone.style.top = y + 'px';
+    clone.style.left = x + 'px';
+
+    document.body.appendChild(clone);
+})
 
 /*
-    1- 
+
+    1- on window click, clone #mouse
+    2- clone append on the click x and y
 
 */
